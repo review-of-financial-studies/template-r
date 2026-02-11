cc4a864adb46bce24a9e4b68c8ad7b16919eb2e3
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2026-02-11 17:04:38 +0100

    Add results/log/main.txt

--- main.R
+++ main.R
@@ -10,13 +10,18 @@ dirs <- c(
   "data/analysis",
   "results",
   "results/figures",
-  "results/tables"
+  "results/tables",
+  "results/log"
 )
 
 for (dir in dirs) {
   dir.create(dir, recursive = TRUE, showWarnings = FALSE)
 }
 
-source("code/download_raw_data.R")
-source("code/create_analysis_data.R")
-source("code/analyze_data.R")
+sink("results/log/main.txt")
+
+source("code/download_raw_data.R", echo = TRUE)
+source("code/create_analysis_data.R", echo = TRUE)
+source("code/analyze_data.R", echo = TRUE)
+
+sink()
